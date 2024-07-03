992c88c5bf7285436cdab81fab6c22f1723f8725
Bhushanmv007 <bhushanmv2003@gmail.com>
2024-07-03 20:23:15 +0530

    organized

---
+++ day3/c++/OOPs/destructor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Student {
+public:
+    int RollNo;
+    string Name;
+    string Branch;
+    Student(int RollNo,string Name,string Branch){
+        this->RollNo = RollNo;
+        this->Name = Name;
+        this->Branch = Branch;
+    }
+    ~Student(){
+      cout <<"Object destructed!!"<<endl;
+    }
+    void studentData(){
+        cout << "Name: " << Name << endl;
+        cout << "RollNo: " << RollNo << endl;
+        cout << "Branch: " << Branch << endl;
+    }
+};
+int main() {
+    Student s1(101, "Sanjay", "CSE");
+    s1.studentData();
+    return 0;
+}
