f833069bd7051f6d99d1c42375dc8fee3db58fb7
Bhushanmv007 <bhushanmv2003@gmail.com>
2024-09-03 10:19:37 +0530

    day 2

---
+++ callByVal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void swap(int a, int b){
+      int temp = a;
+      a=b;
+      b=temp;
+}
+
+int main(){
+      int a=10;
+      int b=20;
+      cout<<"values of a= "<<a<<"& b= "<<b<<" before swap"<<endl;
+      swap(a,b);
+      cout<<"values of a= "<<a<<"& b= "<<b<<" after swap"<<endl;
+}
